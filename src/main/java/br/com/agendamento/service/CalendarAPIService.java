e7ed2d078e6b59daa32bd7c7afb93ed151a5c90e
Marcelo <mdbezerra16@gmail.com>
2025-10-05 15:56:20 -0300

    build: :wrench: Nova forma de acessar as credenciais da API do Calendar
    Autorização por conta de serviço

--- src/main/java/br/com/agendamento/service/CalendarAPIService.java
+++ src/main/java/br/com/agendamento/service/CalendarAPIService.java
@@ -3,29 +3,23 @@ package br.com.agendamento.service;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
@@ -39,34 +33,28 @@ public class CalendarAPIService {
 	private String APPLICATION_NAME;
 	@Value("${CREDENTIALS_FILE_PATH}")
 	private String CREDENTIALS_FILE_PATH;
-	@Value("${TOKENS_DIRECTORY_PATH}")
-	private String TOKENS_DIRECTORY_PATH;
 	@Value("${CALENDAR_ID}")
 	private String CALENDAR_ID;
 	private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-	private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
 
+	@SuppressWarnings("deprecation")
 	private static Credential pegarCredenciais(final NetHttpTransport HTTP_TRANSPORT,
-			String credentialPath, String tokenPath) throws IOException {
+			String credentialPath) throws IOException {
 		InputStream in = AgendamentoApplication.class.getResourceAsStream(credentialPath);
 		if (in == null) {
 			throw new FileNotFoundException("Arquivo não encontrado: " + credentialPath);
 		}
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES)
-				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(tokenPath)))
-				.setAccessType("offline")
-				.build();
-		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-		return credential;
+		    GoogleCredential credential = GoogleCredential
+            .fromStream(in, HTTP_TRANSPORT, JSON_FACTORY)
+            .createScoped(Collections.singleton(CalendarScopes.CALENDAR));
+
+    	return credential;
 	}
 
 	public String cadastrarEvento(LocalDateTime inicio, String nomeUsuario)
 			throws GeneralSecurityException, IOException {
 		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, pegarCredenciais(HTTP_TRANSPORT, CREDENTIALS_FILE_PATH, TOKENS_DIRECTORY_PATH))
+		Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, pegarCredenciais(HTTP_TRANSPORT, CREDENTIALS_FILE_PATH))
 				.setApplicationName(APPLICATION_NAME)
 				.build();
 
